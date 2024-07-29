7464b7575b73c08e9fbb3aba11daec714fe6a805
VK-ston <adria.machin.ee@sarria.salesians.cat>
2024-07-29 21:41:02 +0200

    borrarLibro.php

--- borrarLibro.php
+++ borrarLibro.php
@@ -1,39 +1,32 @@
 <?php
 session_start(); // Inicia la sesión
 
-// Verificar que 'codigo' y 'posicion' están presentes
-if (isset($_GET['codigo']) && $_GET['codigo'] === 'codigolibro2231,' && isset($_GET['posicion'])) {
-    $pos = intval($_GET['posicion']); // Asegúrate de que 'posicion' sea un número entero
 
-    $libros = json_decode(file_get_contents('libros.json'), true);
-    if (isset($libros[$pos])) {
-        // Se borran las imágenes físicamente del sistema de archivos
-        if (isset($libros[$pos]['Portada'])) {
-            $destino = './images/portadas/'.$libros[$pos]['Portada'];
-            if (file_exists($destino)) {
-                unlink($destino);
-            }	
-        }
+if ($_GET['codigo']=='democode1234') {
+$pos = $_GET['posicion'];
+//echo $pos;
+$libros = json_decode(file_get_contents('libros.json'),true);
+// Se borran las imágenes físicamente del sistema de archivos
+if (isset($libros[$pos]['Portada'])) {
+	$destino = './images/portadas/'.$libros[$pos]['Portada'];
+	if (file_exists($destino)) {
+		unlink($destino);
+	}	
+}	
+//echo '<pre>';
+  
+// Eliminar el libro del array
+unset($libros[$pos]);
+//echo 'Después de borrar:<br>';
+// Guardar los cambios en el archivo JSON
+file_put_contents("libros.json",json_encode($libros));
 
-        // Eliminar el libro del array
-        unset($libros[$pos]);
-
-        // Reindexar el array para evitar agujeros en los índices
-        $libros = array_values($libros);
-
-        // Guardar los cambios en el archivo JSON
-        file_put_contents('libros.json', json_encode($libros));
-
-        // Mensaje de éxito en la sesión
-        $_SESSION['mensajeEliminar'] = "Libro eliminado correctamente";
-    } else {
-        $_SESSION['mensajeEliminar'] = "No se encontró el libro para eliminar";
-    }
+// Mensaje de éxito en la sesión
+$_SESSION['mensajeEliminar'] = "Libro eliminado correctamente";
 } else {
     // Establecer el mensaje de notificación de código incorrecto
-    $_SESSION['mensajeCodigoIncorrecto'] = 'Código incorrecto o parámetros faltantes';
+    $_SESSION['mensajeCodigoIncorrecto'] = 'Código incorrecto';
 }
 
 header('Location: index.php');
-exit(); // Asegúrate de que no haya más salida después de la redirección
 ?>
